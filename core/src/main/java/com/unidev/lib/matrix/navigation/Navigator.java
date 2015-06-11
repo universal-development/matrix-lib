34293c2c9df953be623f3eccb6604383e6faca60
Denis O <denis@universal-development.com>
2015-06-11 16:17:40 +0300

    Templates for navigation & pathing

---
+++ core/src/main/java/com/unidev/lib/matrix/navigation/Navigator.java
@@ -0,0 +1,7 @@
+package com.unidev.lib.matrix.navigation;
+
+/**
+ * Matrix navigator, use navigator to implement custom matrix navigation algorithm.
+ */
+public class Navigator {
+}
---
+++ core/src/main/java/com/unidev/lib/matrix/navigation/Path.java
@@ -0,0 +1,7 @@
+package com.unidev.lib.matrix.navigation;
+
+/**
+ * Matrix path, collection of coordinates of path
+ */
+public class Path {
+}
