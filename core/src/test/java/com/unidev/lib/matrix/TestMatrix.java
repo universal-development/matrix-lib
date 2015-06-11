971e04d1aa30ac0532516ea8d29e4f5069318c2c
Denis O <denis@universal-development.com>
2015-06-11 13:29:10 +0300

    added test matrix class

---
+++ core/src/test/java/com/unidev/lib/matrix/TestMatrix.java
@@ -0,0 +1,25 @@
+package com.unidev.lib.matrix;
+
+import com.unidev.lib.matrix.model.Matrix;
+
+/**
+ * Basic class matrix class for testing purposes,
+ * it will store Integer values.
+ */
+public class TestMatrix extends Matrix<Integer> {
+
+    public TestMatrix(Integer dimension) {
+        super(dimension);
+    }
+
+    public TestMatrix(Integer rows, Integer columns) {
+        super(rows, columns);
+    }
+
+    @Override
+    protected Integer[][] createMatrix(Integer rows, Integer columns) {
+        return new Integer[0][];
+    }
+
+
+}
