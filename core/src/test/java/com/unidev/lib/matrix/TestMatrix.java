87c04f07a50afd235e0d323993361a1835c034cc
Denis O <denis@universal-development.com>
2015-06-11 15:56:09 +0300

    get/set test

--- core/src/test/java/com/unidev/lib/matrix/TestMatrix.java
+++ core/src/test/java/com/unidev/lib/matrix/TestMatrix.java
@@ -1,5 +1,6 @@
 package com.unidev.lib.matrix;
 
+import com.unidev.lib.matrix.model.Coordinate;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -15,6 +16,21 @@ public class TestMatrix {
         assertThat(demoMatrix.getRows(), is(3));
         assertThat(demoMatrix.getColumns(), is(3));
 
+        demoMatrix = new DemoMatrix(4, 5);
+
+        assertThat(demoMatrix.getRows(), is(4));
+        assertThat(demoMatrix.getColumns(), is(5));
+
+    }
+
+    @Test
+    public void testElementGetSet() {
+        DemoMatrix demoMatrix = new DemoMatrix(3);
+
+        demoMatrix.set(Coordinate.newInstance().withRow(0).withColumn(1), 666);
+
+        Integer value = demoMatrix.get(Coordinate.newInstance().withRow(0).withColumn(1));
+        assertThat(value, is(666));
     }
 
 
