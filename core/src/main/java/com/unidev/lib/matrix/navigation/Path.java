bdd7e46093fe654cd4fbd59c7c5e4a1772ffd090
Denis O <denis@universal-development.com>
2015-06-19 16:34:50 +0300

    basic matrix path object

--- core/src/main/java/com/unidev/lib/matrix/navigation/Path.java
+++ core/src/main/java/com/unidev/lib/matrix/navigation/Path.java
@@ -1,7 +1,41 @@
 package com.unidev.lib.matrix.navigation;
 
+import com.unidev.lib.matrix.model.Coordinate;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Matrix path, collection of coordinates of path
  */
 public class Path {
+
+    protected List<Coordinate> path;
+
+    public Path() {
+        path = new ArrayList<Coordinate>();
+    }
+
+    public Path(List<Coordinate> path) {
+        this.path = path;
+    }
+
+    public void add(Coordinate coordinate) {
+        path.add(coordinate);
+    }
+
+    public List<Coordinate> getPath() {
+        return path;
+    }
+
+    public void clear() {
+        path.clear();
+    }
+
+    @Override
+    public String toString() {
+        return "Path{" +
+                "path=" + path +
+                '}';
+    }
 }
